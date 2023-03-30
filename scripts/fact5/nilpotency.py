f445441a0be5cfdd2ca02ad6dd99c7c641b91184
caklovicka <caklovicka@gmail.com>
2023-03-30 20:07:12 +0200

    nilpotency

---
+++ scripts/fact5/nilpotency.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 21:39:39 2023
+
+@author: telu
+"""
+import numpy as np
+
+from pycode.qmatrix import genCollocation
+
+# change these:
+############################
+M = 4
+quadType = 'GAUSS'
+distr = 'LEGENDRE'
+############################
+
+def spectralRadius(x):
+    x = np.asarray(x)
+    R = Q - np.diag(x)
+    return np.max(np.abs(np.linalg.eigvals(R)))
+
+nodes, _, Q = genCollocation(M, distr, quadType)
+
+print('quadType = {}\ndistr = {}\nM={}'.format(quadType, distr, M))
+print('define: D = diag(nodes) / M\n')
+D = np.diag(nodes) / M
+
+S = (Q- D)
+powS = np.eye(M)
+for m in range(M+1):
+    print('|(Q - D)^{}|_max = {}'.format(m, np.max(powS)))
+    powS = powS @ S
+
+
+
+
+
+
---
+++ scripts/fact5/pycode/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 17:42:50 2023
+
+@author: cpf5546
+"""
+import matplotlib.pyplot as _plt
+
+# General matplotlib settings
+_plt.rc('font', size=12)
+_plt.rcParams['lines.linewidth'] = 2
+_plt.rcParams['axes.titlesize'] = 18
+_plt.rcParams['axes.labelsize'] = 16
+_plt.rcParams['xtick.labelsize'] = 16
+_plt.rcParams['ytick.labelsize'] = 16
+_plt.rcParams['xtick.major.pad'] = 5
+_plt.rcParams['ytick.major.pad'] = 5
+_plt.rcParams['axes.labelpad'] = 6
+_plt.rcParams['markers.fillstyle'] = 'none'
+_plt.rcParams['lines.markersize'] = 7.0
+_plt.rcParams['lines.markeredgewidth'] = 1.5
+_plt.rcParams['mathtext.fontset'] = 'cm'
+_plt.rcParams['mathtext.rm'] = 'serif'
+_plt.rcParams['figure.max_open_warning'] = 100
