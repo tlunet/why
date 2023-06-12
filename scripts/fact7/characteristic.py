246a47e297df8aa2adc5319a89f1ff423164f19d
Thibaut Lunet <thibaut.lunet@tuhh.de>
2023-06-12 18:42:25 +0200

    TL: added script for determinant of Q

--- scripts/fact7/characteristic.py
+++ scripts/fact7/characteristic.py
@@ -17,31 +17,32 @@ fullSym = True
 
 nodes, _, QNum = genCollocation(M, distr, quadType)
 
+nCoeffs = M
 if quadType in ['LOBATTO', 'RADAU-LEFT']:
-    M -= 1;
+    nCoeffs -= 1;
     QNum = QNum[1:, 1:]
     nodes = nodes[1:]
-    
-qDetNum = np.prod(nodes)/sp.special.factorial(M)
-    
 
-qCoeffs = [[sy.Symbol('q_{'+str(i)+','+str(j)+'}') 
-            for i in range(M)] 
-           for j in range(M)]
+qDetNum = (-1)**(M+1) * np.prod(nodes)/sp.special.factorial(M)
+
+
+qCoeffs = [[sy.Symbol('q_{'+str(i)+','+str(j)+'}')
+            for i in range(nCoeffs)]
+           for j in range(nCoeffs)]
 
 if fullSym:
     Q = sy.Matrix(qCoeffs)
 else:
     Q = sy.Matrix(QNum)
 
-xCoeffs = [sy.Symbol(f'x_{i}') for i in range(M)]
+xCoeffs = [sy.Symbol(f'x_{i}') for i in range(nCoeffs)]
 
 D = sy.diag(*xCoeffs)
 
 lam = sy.Symbol('\\lambda')
 
-Korig =  D.inv()*Q - sy.eye(M)
-K = D.inv()*Q - (1+lam)*sy.eye(M)
+Korig =  D.inv()*Q - sy.eye(nCoeffs)
+K = D.inv()*Q - (1+lam)*sy.eye(nCoeffs)
 
 print('Computing determinant')
 poly = sy.poly(K.det(), lam)
@@ -60,7 +61,7 @@ qDet = sy.Symbol('\\Delta')
 if not fullSym:
     print('Solving symbolically')
     sol = sy.solve(equations, *xCoeffs)
-    
+
     if len(sol) > 0:
         print('Found solution(s) symbolically')
     else:
@@ -68,24 +69,22 @@ if not fullSym:
         sol = [sy.nsolve(equations, xCoeffs, nodes)]
         if len(sol) > 0:
             print('Found solution numerically')
-    
-    
+
+
     sol = [np.array(c, dtype=float).ravel() for c in sol]
     # AHAHA we beat it !
     # sol += [np.array([0.2865524188780046, 0.11264992497015984, 0.2583063168320655])]
-    
+
     if len(sol) > 0:
         for xCoeffNum in sol:
             print(f'diagonal coefficients : {xCoeffNum}')
             DNumInv = np.diag(1/xCoeffNum)
-            
-            KNum = np.eye(M) - DNumInv @ QNum
-            
-            KPow = np.eye(M)
-            for m in range(M):
+
+            KNum = np.eye(nCoeffs) - DNumInv @ QNum
+
+            KPow = np.eye(nCoeffs)
+            for m in range(nCoeffs):
                 KPow = KNum @ KPow
                 print('|prod(I - Dinv @ Q)|_max = {:<7.4e}, m = 1, ..., {:<3}        ro(prod) = {}'.format(
                     np.max(np.abs(KPow)), m+1, max(np.abs(np.linalg.eigvals(KPow)))))
                 print(np.linalg.cond(KPow))
-    
-    
---
+++ scripts/fact7/determinant.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 18:26:32 2023
+
+@author: cpf5546
+"""
+import numpy as np
+import scipy as sp
+from pycode.qmatrix import genCollocation
+
+M = 3
+quadType = 'RADAU-LEFT'
+distr = 'LEGENDRE'
+
+nodes, _, QFull = genCollocation(M, distr, quadType)
+
+Q = QFull
+if quadType in ['LOBATTO', 'RADAU-LEFT']:
+    Q = Q[1:, 1:]
+    nodes = nodes[1:]
+
+qDetTh = (-1)**(M+1) * np.prod(nodes)/sp.special.factorial(M)
+
+qDetNum = np.linalg.det(Q)
+
+print(f'M={M}, {quadType}-{distr}')
+print(f' -- detTh  : {qDetTh}')
+print(f' -- detNum : {qDetNum}')
