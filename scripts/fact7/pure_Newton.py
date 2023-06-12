148b4dff6feae839abe0f316c091378ef9a1e6a7
Gaya <caklovicka@gmail.com>
2023-06-12 21:46:24 +0200

    purely numerical Newton with M equations

---
+++ scripts/fact7/pure_Newton.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  9 17:10:18 2023
+
+@author: telu
+"""
+import numpy as np
+import scipy as sp
+from pycode.qmatrix import genCollocation
+
+def f(x):
+    c = 1
+    y = np.empty_like(x)
+    K = np.diag(1/np.array(x)) @ Q - np.eye(M)
+    for z in range(M):
+        y[z] = np.linalg.det(c * np.eye(M) + c/(z + 1) * K) - c ** M
+    return y
+
+
+M = 5
+quadType = 'GAUSS'
+distr = 'LEGENDRE'
+
+
+# RADAU-RIGHT, GAUSS until 5
+# RADAU-LEFT until 4
+
+nodes, _, Q = genCollocation(M, distr, quadType)
+if quadType in ['LOBATTO', 'RADAU-LEFT']:
+    Q = Q[1:, 1:]
+    nodes = nodes[1:]
+    M = M-1
+
+sol = sp.optimize.root(f, nodes, tol=1e-16, method='hybr')
+print(sol)
+x = sol.x
+print()
+print('x = ', x)
+K = np.diag(1/x) @ Q - np.eye(M)
+Kpow = np.eye(M)
+for i in range(M):
+    Kpow = K @ Kpow
+    print('|prod(I - Dinv @ Q)|_max = {:<7.4e}, m = 1, ..., {:<3}   ro(prod) = {}'.format(
+        np.max(np.abs(Kpow)), i + 1, max(np.abs(np.linalg.eigvals(Kpow)))))
+
+
+
